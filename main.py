c0ac3da7fdc7f4ad2bf1fd60a73379659b641f16
Jacob van Steyn <jacobvs2@gmail.com>
2022-02-14 06:10:42 -0500

    Changed crossover function to use different method

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import random
 
+import numpy as np
+
 from genetic import GeneticAlgorithm
 from NumberAlloc import NumberAlloc
+from puzzle2 import puzzle2Piece
 
 
-def create_test_file(filename):
+def create_test_file(filename, is_problem_1: bool):
     # Create a file with the given filename
     # The file contains 40 random integers between -10 and 10, each on a separate line
     # The file is created in the current directory
     # The file is not returned
     with open(filename, 'w') as f:
-        for i in range(40):
-            f.write(str(random.randint(-10, 10)) + '\n')
+        if is_problem_1:
+            for i in range(40):
+                f.write(str(random.randint(-10, 10)) + '\n')
+        else:
+            for i in range(random.randint(3, 10)):
+                type = np.random.choice(['Door', 'Wall', 'Lookout'])
+                width = random.randint(1, 9)
+                strength = random.randint(1, 9)
+                cost = random.randint(1, 9)
+                f.write(f"{type}\t{width}\t{strength}\t{cost}\n")
 
 
-
-# create_test_file('test.txt')
+# create_test_file('problem2.txt', False)
 
 def get_numbers(filename):
     # Read an input file with 40 numbers on each line
@@ -27,11 +37,29 @@ def get_numbers(filename):
             numbers.append(int(line))
     return numbers
 
-nums = get_numbers('test.txt')
+def get_pieces(filename):
+    with open(filename, 'r') as f:
+        pieces = []
+        for line in f:
+            attrs = line.split('\t')
+            piece = puzzle2Piece(attrs[0], int(attrs[1]), int(attrs[2]), int(attrs[3]))
+            print(piece)
+            pieces.append(piece)
+        return pieces
 
-GA = GeneticAlgorithm(numbers=nums, population_size=20, mutation_rate=-1, tournament_size=4,
-                      use_elitism=False, use_culling=False, max_time=10)
-best = GA.run()
+# nums = get_numbers('test.txt')
+#
+# GA = GeneticAlgorithm(data=nums, population_size=20, mutation_rate=0.15, tournament_size=4,
+#                       use_elitism=False, use_culling=False, max_time_seconds=10)
+# best = GA.run()
+#
+# # Print Best Individual
+# print(f"Best Individual:\n\tFitness: {best.fitness:,}\n\tBins: {best.bins}")
+
+pieces = get_pieces('problem2.txt')
+GA2 = GeneticAlgorithm(data=pieces, population_size=20, mutation_rate=0.2,
+                       use_elitism=True, use_culling=True, max_time_seconds=10)
+best2 = GA2.run()
 
 # Print Best Individual
-print(f"Best Individual:\n\tFitness: {best.fitness:,}\n\tBins: {best.bins}")
+print(f"Best Individual:\n\tFitness: {best2.fitness:,}\n\tStack: {best2}")
--- puzzle2.py
+++ puzzle2.py
@@ -2,10 +2,11 @@ import enum
 
 from genetic import *
 
-class pieceType(enum.Enum):
-    Door = "Door"
-    Wall = "Wall"
-    Lookout = "Lookout"
+# class pieceType(enum.Enum):
+#     Door = "Door"
+#     Wall = "Wall"
+#     Lookout = "Lookout"
+
 
 class puzzle2Piece:
     """
@@ -17,55 +18,108 @@ class puzzle2Piece:
         self.strength = strength
         self.cost = cost
 
-class tower:
+    def __eq__(self, other):
+        if self.pieceType == other.pieceType and self.width == other.width and \
+            self.strength == other.strength and self.cost == other.cost:
+            return True
+        return False
+
+    def __hash__(self):
+        return hash((self.pieceType, self.width, self.strength, self.cost))
+
+    def __str__(self):
+        return f"{self.pieceType}, {self.width}, {self.strength}, {self.cost}\n"
+
+
+class Tower:
     """
     Class for tower
     Consists of n tower pieces stored in a list
     Last index in the list being the last piece
     """
-    def __init__(self, listOfPieces, isValid):
+
+    def __init__(self, listOfPieces, allPieces):
         self.listOfPieces = listOfPieces
-        self.isValid = isValid;
+        self.allPieces = allPieces
+        self.isValid = isTowerValid(listOfPieces)
+        self.fitness = self.calcTowerScore()
+
+    def __str__(self):
+        return ''.join(str(piece) for piece in self.listOfPieces)
+
+    def calcTowerScore(self) -> float:
+        """returns the tower's fitness"""
+        if self.isValid is False:
+            return 0
+        return max(0, 10 + (len(self.listOfPieces) ** 2) - self.findTowerCost())
+
+    def findTowerCost(self) -> float:
+        totalCost = 0
+        for puzzle2Piece in self.listOfPieces:
+            totalCost += puzzle2Piece.cost
+        return totalCost
+
+    # def calcTowerFitness(self) -> float:
+    #     " add +10 to the score for each piece not on the end which is a wall"
+    #     fitness = self.findTowerCost()
+    #     nonwall = len([x.pieceType != 'Wall' for x in self.listOfPieces])
+    #     fitness -= nonwall
+    #     fitness += len(self.listOfPieces) if self.listOfPieces[-1].pieceType == "Lookout" else 0
+    #     fitness += len(self.listOfPieces) if self.listOfPieces[0].pieceType == "Door" else 0
+    #     return fitness
+
+    def mutate(self, mutation_rate):
+        """mutates the tower"""
+        if random.random() < mutation_rate:
+            # Either Add or Remove a piece
+            remaining = set(self.allPieces) - set(self.listOfPieces)
+            if (len(remaining) == 0 or random.random() < 0.5) and len(self.listOfPieces) > 1:
+                # Remove a random piece
+                self.listOfPieces.pop(random.randint(0, len(self.listOfPieces) - 1))
+            else:
+                # Add a random piece
+                self.listOfPieces.append(random.choice(list(remaining)))
+
+        for i, piece in enumerate(self.listOfPieces):
+            if random.random() < mutation_rate:
+                # move the piece somewhere else
+                new_index = random.randint(0, len(self.listOfPieces) - 1)
+                self.listOfPieces[i], self.listOfPieces[new_index] = self.listOfPieces[new_index], self.listOfPieces[i]
+
+        self.isValid = isTowerValid(self.listOfPieces)
+        self.fitness = self.calcTowerScore()
 
 
-def findTowerCost(towerUsed) -> float:
-    totalCost = 0
-    for puzzle2Piece in towerUsed:
-        totalCost + puzzle2Piece.cost
-    return totalCost
 
 
-def isTowerValid(listOfUsedPieces):
+def isTowerValid(listOfUsedPieces: typing.List[puzzle2Piece]) -> bool:
     """
     Determines if the tower is valid, considering project specifications
     """
-    lastPiece = listOfUsedPieces[0]  # initialize first piece for width check
     # loop throughout tower pieces to make sure there isn't a larger piece on top of a smaller one
     # and each piece can hold the weight above it
-
+    lastPiece = None
     listSize = len(listOfUsedPieces)
 
-    for i, puzzle2Piece in enumerate(listOfUsedPieces):
-        #  make sure this piece is not wider than the previous
-        if puzzle2Piece.width > lastPiece.width:
-            return False
-        lastPiece = puzzle2Piece;
+    for i, piece in enumerate(listOfUsedPieces, start=1):
+        if i != 1:
+            #  make sure this piece is not wider than the previous
+            if piece.width > lastPiece.width:
+                return False
 
-        #  make this piece can hold the rest of the building
-        if (listSize-(i+1)) > puzzle2Piece.strength:
-            return False
+            #  make this piece can hold the rest of the building
+            if (listSize-i) > piece.strength:
+                return False
+        lastPiece = piece
 
     #  make sure the top and bottom are correct and all pieces inbetween are walls
     if not listOfUsedPieces[-1].pieceType == "Lookout" or not listOfUsedPieces[0].pieceType == "Door":
         return False
 
-    if any(x.pieceType != "Wall" for x in listOfUsedPieces[1:-1]): return False
+    if any(x.pieceType != "Wall" for x in listOfUsedPieces[1:-1]):
+        return False
 
     return True
 
 
-def calcTowerScore(towerUsed) -> float:
-    """returns the tower's fitness"""
-    if not towerUsed.isValid:
-        return 0
-    return 10 + (len(towerUsed.listOfPieces))**2 + findTowerCost(towerUsed)
+
